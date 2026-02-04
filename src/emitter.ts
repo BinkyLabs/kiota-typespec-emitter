c8d8c8d7aa3ac6d02f012f5bfb684ac8e19ee6c7
Vincent Biret <vincentbiret@hotmail.com>
2026-02-04 13:41:13 -0500

    draft: prototype kiota client generation
    Signed-off-by: Vincent Biret <vincentbiret@hotmail.com>

--- src/emitter.ts
+++ src/emitter.ts
@@ -1,5 +1,6 @@
 import { EmitContext, emitFile, resolvePath } from "@typespec/compiler";
 import { $onEmit as openApiOnEmit } from "@typespec/openapi3";
+import { ConsumerOperation, generateClient, KiotaGenerationLanguage } from "@microsoft/kiota";
 
 export async function $onEmit(context: EmitContext) {
   await emitFile(context.program, {
@@ -14,5 +15,22 @@ export async function $onEmit(context: EmitContext) {
       "omit-unreachable-types": true,
       "openapi-versions": ["3.2.0"],
     } as any, // the any cast is needed because the versions is internal, remove when https://github.com/microsoft/typespec/pull/9584 is published
+  });
+
+  // check that the file was created
+  const openApiFilePath = resolvePath(context.emitterOutputDir, "openapi.json");
+  const openApiFile = await context.program.host.readFile(openApiFilePath);
+  if (!openApiFile) {
+    throw new Error("OpenAPI file was not emitted, check the logs for errors.");
+  }
+
+  await generateClient({
+    openAPIFilePath: openApiFilePath,
+    outputPath: resolvePath(context.emitterOutputDir, "kiota-client"),
+    clientClassName: "KiotaClient",
+    language: KiotaGenerationLanguage.CSharp,
+    clientNamespaceName: "KiotaGeneratedClient",
+    operation: ConsumerOperation.Generate,
+    workingDirectory: context.emitterOutputDir,
   });
 }
